17ac974b439cb00a7920c49e7ea2af2abe61b838
Geanfranco Biagi <118919877+gbiagi@users.noreply.github.com>
2025-08-04 11:36:45 +0200

    Initial commit

---
+++ src/main/java/sdev/mediamasterserver/MediaMasterServerApplication.java
@@ -0,0 +1,13 @@
+package sdev.mediamasterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaMasterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaMasterServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sdev/mediamasterserver/MediaMasterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sdev.mediamasterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaMasterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
